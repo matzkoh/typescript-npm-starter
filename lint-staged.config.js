70b98734a8bd7f87c614528387b0a361019c0aff
matzkoh <matzkoh@gmail.com>
2020-01-28 16:31:16 +0900

    chore: bump deps

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,4 @@
-const add = 'git add'
-const eslint = 'eslint --fix'
-const prettier = 'prettier --write'
-
 module.exports = {
-  '*.{js,ts}': [eslint, add],
-  '*.{json,md,yml}': [prettier, add],
+  '*.{js,ts}': 'eslint --fix',
+  '*.{json,md,yml}': 'prettier --write',
 }
